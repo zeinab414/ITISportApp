52452b5044e4730a9c68590c16255f45e6bdcf14
Hend Hamed <hendhamedmorsy@gmail.com>
2022-05-11 20:53:57 +0200

    UI_Version1

---
+++ ITISportApp/FavoritViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FavoritViewController.swift
+//  ITISportApp
+//
+//  Created by user189294 on 5/11/22.
+//  Copyright © 2022 ititeam. All rights reserved.
+//
+
+import UIKit
+
+class FavoritViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ITISportApp/LeagesTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  LeagesTableViewCell.swift
+//  ITISportApp
+//
+//  Created by user189294 on 5/11/22.
+//  Copyright © 2022 ititeam. All rights reserved.
+//
+
+import UIKit
+
+class LeagesTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var leagsImage: UIImageView!
+    
+    @IBOutlet weak var leagsName: UILabel!
+}
